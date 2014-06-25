632bdbe277180ce3caa87f803dbe33ee825dacc1
Jean-Rémy Falleri <jr.falleri@gmail.com>
2014-06-25 18:27:32 +0200

    added metadata analysis that puts everything in mongodb.

---
+++ includes/metadata.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+require 'mongo'
+
+class MetadataAnalysis < GritAnalysis
+
+	def initialize(source, repo, options, glob)
+		super
+		client = Mongo::MongoClient.new
+		db = client[@options['mongo']['database']]
+		@col = db['commits']
+	end
+
+	def run
+		walker = Rugged::Walker.new(@repo)
+		walker.sorting(Rugged::SORT_DATE)
+		walker.push(@repo.last_commit)
+		walker.each{ |c|
+			commit = Hash.new
+			commit['source'] = @source
+			commit['oid'] = c.oid
+			commit['message'] = c.message
+			commit['author'] = c.author
+			commit['committer'] = c.committer
+			@col.insert(commit)
+		}
+	end
+
+end
