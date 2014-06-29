fd2b8e672c89086870ba0d6ecdffe6e799410ac2
Jean-Rémy Falleri <jr.falleri@laposte.net>
2014-06-29 12:15:48 +0200

    added db addon

---
+++ includes/db.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+require 'mongo'
+
+class Db < Addon
+
+	attr_reader :db
+
+	def initialize(*args)
+		super
+		client = Mongo::MongoClient.new
+		@db = client[@options['mongo']['database']]
+	end
+
+	def name
+		'db'
+	end
+
+end
