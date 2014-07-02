0ea5d7da8b580a2af5ec31f510b44af3c93f762e
Jean-Rémy Falleri <jr.falleri@laposte.net>
2014-07-02 09:41:27 +0200

    simplified ruby code using better idioms.

--- includes/analyses/cloc.rb
+++ includes/analyses/cloc.rb
@@ -6,7 +6,7 @@ class ClocAnalysis < Analysis
 
 	def run
 		cloc = `cloc . --progress-rate=0 --quiet --yaml`
-		if !"".eql?(cloc)
+		unless cloc.empty?
 			yaml = YAML.load(cloc.lines[2..-1].join)
 			yaml.delete('header')
 			output = { :source => @source, :cloc => yaml }
