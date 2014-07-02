9f956847ee34720bdb7ed7ccbd79a27f9fcd0b07
Jean-Rémy Falleri <jr.falleri@laposte.net>
2014-07-02 09:41:57 +0200

    now metadata analysis also saves tags and branches

--- includes/analyses/metadata.rb
+++ includes/analyses/metadata.rb
@@ -3,19 +3,31 @@
 class MetadataAnalysis < Analysis
 
 	def run
-		col = @addons['db'].db['commits']
-	
+		# Importing tags
+		tags = @addons['db'].db['tags']
+		@repo.tags.each{ |t|
+			tag = { source: @source, name: t.name, target: t.target.oid }
+			tags.insert(tag)
+		}
+
+		# Importing branches
+		branches = @addons['db'].db['branches']
+		@repo.branches.each{ |b|
+			branch = { source: @source, name: b.name, target: b.target.oid }
+			branches.insert(branch)
+		}
+
+		# Importing commits
+		commits = @addons['db'].db['commits']
 		walker = Rugged::Walker.new(@repo)
 		walker.sorting(Rugged::SORT_DATE)
 		walker.push(@repo.last_commit)
 		walker.each{ |c|
-			commit = Hash.new
-			commit['source'] = @source
-			commit['oid'] = c.oid
-			commit['message'] = c.message
-			commit['author'] = c.author
-			commit['committer'] = c.committer
-			col.insert(commit)
+			commit = {
+				source: @source, oid: c.oid, message: c.message, author: c.author,
+				committer: c.committer, parent_ids: c.parent_ids, time: c.time
+			}
+			commits.insert(commit)
 		}
 	end
 
