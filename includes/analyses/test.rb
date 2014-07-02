30c239fee11c1a87041ba815be30135983243deb
Jean-Rémy Falleri <jr.falleri@laposte.net>
2014-07-02 09:41:00 +0200

    test analysis is now just an empty shell.

--- includes/analyses/test.rb
+++ includes/analyses/test.rb
@@ -3,14 +3,7 @@
 class TestAnalysis < Analysis
 
 	def run
-		puts @repo.tags.each_name.to_a.inspect
-		
-		walker = Rugged::Walker.new(@repo)
-		walker.sorting(Rugged::SORT_DATE)
-		walker.push(@repo.last_commit)
-		walker.each{ |c|
-			#puts c.inspect
-		}
+		puts "This analysis is a test."
 	end
 
 end
